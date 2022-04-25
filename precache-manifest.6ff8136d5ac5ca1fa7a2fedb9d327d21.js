ada6a76b8cb2cd9ce01d0d604168fd2705c39415
santa <santoshdvc@gmail.com>
2022-04-25 23:53:18 +0530

    Updates

---
+++ precache-manifest.6ff8136d5ac5ca1fa7a2fedb9d327d21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47826d51ac8e8aa3252dab6800a9e21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99f8dc7c8ee22f56031e",
+    "url": "/static/css/main.2b7027f9.chunk.css"
+  },
+  {
+    "revision": "558b4774143183d4f4fb",
+    "url": "/static/js/2.64416db4.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.64416db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f8dc7c8ee22f56031e",
+    "url": "/static/js/main.e3000ba6.chunk.js"
+  },
+  {
+    "revision": "e036506db9438cd7be18",
+    "url": "/static/js/runtime-main.a219aeb0.js"
+  },
+  {
+    "revision": "d6866835874f3a6f4c993f2c63062f97",
+    "url": "/static/media/agriculture.d6866835.jpg"
+  },
+  {
+    "revision": "3e6595fca6b4e362e6e862472ad6ab7c",
+    "url": "/static/media/avatar2.3e6595fc.jpg"
+  },
+  {
+    "revision": "78eaa187fad39ea7c9426d0bc750abcb",
+    "url": "/static/media/coffeelap.78eaa187.jpg"
+  },
+  {
+    "revision": "af85c9fdc00884daacc9d3299143a243",
+    "url": "/static/media/grasshopper.af85c9fd.png"
+  },
+  {
+    "revision": "b6a0eae659e35edfdb2cb014c678c36f",
+    "url": "/static/media/mwdiary.b6a0eae6.png"
+  }
+]);
